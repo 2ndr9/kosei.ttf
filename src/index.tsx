8e19df7307d7b07d8ea8e4aedf9494df855c81b7
2ndr9 <rk17tnd@gmail.com>
2021-12-14 01:31:19 +0900

    とりあえず

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Canvas from "./canvas";
+import View from "./view";
 
 ReactDOM.render(
   <React.StrictMode>
     <Canvas length={800}></Canvas>
+    <View />
   </React.StrictMode>,
   document.getElementById("root")
 );
